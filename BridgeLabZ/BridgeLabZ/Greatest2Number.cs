8ddf14f75d7905bb4c3cf3be9027a85d8346cdf1
alamdilshad87 <dilshadand@gmail.com>
2025-11-08 15:28:19 +0530

    Conditionals

---
+++ BridgeLabZ/BridgeLabZ/Greatest2Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ
+{
+    internal class Greatest2Number
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                Console.WriteLine("The greatest number is: " + num1);
+            }
+            else
+            {
+                Console.WriteLine("The greatest number is: " + num2);
+            }
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year:");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
