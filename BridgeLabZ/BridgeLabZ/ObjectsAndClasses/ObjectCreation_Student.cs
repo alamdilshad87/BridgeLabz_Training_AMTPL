554e791063449936a87932d7b47d932181d8f85b
alamdilshad87 <dilshadand@gmail.com>
2025-11-08 15:51:14 +0530

    Classes And Objects

---
+++ BridgeLabZ/BridgeLabZ/ObjectsAndClasses/ObjectCreation_Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.ObjectsAndClasses
+{
+    internal class ObjectCreation_Student
+    {
+        public string Name;
+        public int RollNo;
+
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}\nRoll No: {RollNo}\n");
+        }
+        public static void Main()
+        {
+            ObjectCreation_Student s1 = new ObjectCreation_Student();
+            s1.Name = "Dilshad";
+            s1.RollNo = 917;
+
+            ObjectCreation_Student s2 = new ObjectCreation_Student();
+            s2.Name = "Anupam";
+            s2.RollNo = 923;
+
+            s1.Show();
+            s2.Show();
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/ObjectsAndClasses/ObjectWithMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.ObjectsAndClasses
+{
+    internal class ObjectWithMethods
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        public void SetDetails(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID: {id}\n Name: {name}\n Salary: Rs.{salary}\n");
+        }
+        public static void Main()
+        {
+            ObjectWithMethods emp = new ObjectWithMethods();
+            emp.SetDetails(101, "Dilshad", 50000);
+            emp.Display();
+        }
+    }
+}
