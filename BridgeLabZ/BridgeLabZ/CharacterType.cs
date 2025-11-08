4f811b2463489df289859e9d49cedf52b37f4e00
alamdilshad87 <dilshadand@gmail.com>
2025-11-08 15:22:45 +0530

    Character

---
+++ BridgeLabZ/BridgeLabZ/CharacterType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ
+{
+    internal class CharacterType
+    {
+        static void Main()
+        {
+            Console.Write("Enter a character: ");
+            char character = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (char.IsLetter(character))
+            {
+                Console.WriteLine($"{character} is a letter.");
+            }
+            else if (char.IsDigit(character))
+            {
+                Console.WriteLine($"{character} is a digit.");
+            }
+            else if (char.IsWhiteSpace(character))
+            {
+                Console.WriteLine($"{character} is a whitespace character.");
+            }
+            else
+            {
+                Console.WriteLine($"{character} is a special character.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
