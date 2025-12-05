e4b5b71f940727f2fa48d7ca6365dd6086de52d5
alamdilshad87 <dilshadand@gmail.com>
2025-12-05 14:24:59 +0530

    Exception Handling

---
+++ BridgeLabZ/BridgeLabZ/ExceptionHandling/DivideByZero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.ExceptionHandling
+{
+    internal class DivideByZero
+    {
+        static void Main()
+        {
+            try
+            {
+                int a = 10, b = 0;
+                int result = a / b;
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero. " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block always executes.");
+            }
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/ExceptionHandling/InvalidAgeException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.ExceptionHandling
+{
+    class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message) : base(message) { }
+    }
+
+    class CustomException
+    {
+        static void Main()
+        {
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age < 18)
+                    throw new InvalidAgeException("Age must be 18 or above.");
+
+                Console.WriteLine("Access Granted!");
+            }
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine("Custom Exception: " + ex.Message);
+            }
+        }
+    }
+}
