1ffdb0d2b60661b7e71e120744b7dc9cdac5bbc2
alamdilshad87 <dilshadand@gmail.com>
2025-12-05 15:43:02 +0530

    Searching

---
+++ BridgeLabZ/BridgeLabZ/Searching/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.Searching
+{
+    internal class BinarySearch
+    {
+        static int BS(int[] arr, int target)
+        {
+            int left = 0, right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid] == target) return mid;
+                else if (arr[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            }
+            return -1;
+        }
+
+        static void Main()
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            Console.WriteLine(BS(arr, 30));
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/Searching/LinearSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.Searching
+{
+    internal class LinearSearch
+    {
+        static void Main()
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int target = 30;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] == target)
+                    Console.WriteLine("Found at index " + i);
+        }
+    }
+}
