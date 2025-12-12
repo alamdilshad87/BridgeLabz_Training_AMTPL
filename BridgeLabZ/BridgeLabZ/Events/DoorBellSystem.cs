e0f89f16fe192449ff3ba9fd4ce043f1a8f12f25
alamdilshad87 <dilshadand@gmail.com>
2025-12-12 11:00:30 +0530

    Events

---
+++ BridgeLabZ/BridgeLabZ/Events/DoorBellSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.Events
+{
+    internal class DoorBellSystem
+    {
+        public delegate void DoorBellHandler();
+        class Door
+        {
+            public event DoorBellHandler BellRang;
+
+            public void RingBell()
+            {
+                Console.WriteLine("Bell Pressed");
+                BellRang?.Invoke();
+            }
+        }
+
+        class Person
+        {
+            public string _name;
+            public Person(string name)
+            {
+                _name = name;
+            }
+
+            public void Respond()
+            {
+                Console.WriteLine($"{_name} says : Coming to open the door");
+            }
+        }
+
+        class Program
+        {
+            public static void Main()
+            {
+                Person p1 = new Person("Dilshad");
+                Person p2 = new Person("Pranav");
+                Person p3 = new Person("Gaurav");
+
+                Door door = new Door();
+                door.BellRang += p1.Respond;
+                door.BellRang += p2.Respond;
+                door.BellRang += p3.Respond;
+                door.RingBell();
+                door.BellRang -= p2.Respond;
+                door.RingBell();
+            }
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/Events/SimpleEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.Events
+{
+    internal class SimpleEvent
+    {
+        public delegate void Notify();
+
+        class Process
+        {
+            public event Notify ProcessCompleted;
+
+            public void Start()
+            {
+                Console.WriteLine("Process Started!");
+                ProcessCompleted?.Invoke();
+            }
+        }
+
+        class Subscriber
+        {
+            public void OnProcessCompleted()
+            {
+                Console.WriteLine("Process Completed Event Received");
+            }
+        }
+
+        class Program
+        {
+            public static void Main()
+            {
+                Process p = new Process();
+                Subscriber s = new Subscriber();
+
+                p.ProcessCompleted += s.OnProcessCompleted;
+
+                p.Start();
+            }
+        }
+    }
+}
