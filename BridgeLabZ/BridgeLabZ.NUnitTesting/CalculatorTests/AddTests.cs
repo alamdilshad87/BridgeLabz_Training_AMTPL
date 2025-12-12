74dbd16d9ab774598b5831a99b95c5d658f2d784
alamdilshad87 <dilshadand@gmail.com>
2025-12-12 12:02:39 +0530

    Nunit

--- BridgeLabZ/BridgeLabZ.NUnitTesting/CalculatorTests/AddTests.cs
+++ BridgeLabZ/BridgeLabZ.NUnitTesting/CalculatorTests/AddTests.cs
@@ -2,6 +2,7 @@
 
 namespace BridgeLabZ.NUnitTesting.CalculatorTests
 {
+    [TestFixture]
     public class AddTests
     {
         private Calculator calc;
@@ -18,6 +19,18 @@ namespace BridgeLabZ.NUnitTesting.CalculatorTests
             int result = calc.Add(10, 20);
 
             Assert.That(result, Is.EqualTo(30));
+        }
+    }
+    public class MultipleAddTests
+    {
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(-10, -10, -20)]
+
+        public void Add_MultipleTest(int a, int b, int expected)
+        {
+            var calc = new BridgeLabZ.Calculator();
+            Assert.That(calc.Add(a, b), Is.EqualTo(expected));
         }
     }
 }
