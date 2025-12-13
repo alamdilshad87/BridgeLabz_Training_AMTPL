acbecaa17b23a2b8c064be5bd92d9728584bc5d7
alamdilshad87 <dilshadand@gmail.com>
2025-12-13 15:30:02 +0530

    Events

---
+++ BridgeLabZ/BridgeLabZ/Reflections/SimpleReflection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BridgeLabZ.Reflections
+{
+    internal class SimpleReflection
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public void Study() => Console.WriteLine("Studying");
+        }
+
+        class Program
+        {
+            public static void Main()
+            {
+                Type t = typeof(Student);
+                Console.WriteLine("Class Name:" + t.Name);
+                Console.WriteLine("Properties");
+                foreach (var p in t.GetProperties())
+                    Console.WriteLine(p.Name);
+
+                Console.WriteLine("Methods:");
+                foreach (var m in t.GetMethods())
+                    Console.WriteLine(m.Name);
+            }
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/Reflections/StudentReflection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ.Reflections
+{
+    internal class StudentReflection
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Student() { }
+            public Student(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public void Study() => Console.WriteLine($"{Name} is Studying");
+            public void ShowDetails() => Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+        public class Program
+        {
+            public static void Main()
+            {
+                Type t = typeof(Student);
+
+                Console.WriteLine("Class name: " + t.Name);
+                Console.WriteLine("Namespace:" + t.Namespace);
+
+                Console.WriteLine("\nProperties ->");
+                foreach (var prop in t.GetProperties())
+                {
+                    Console.WriteLine($"Property: {prop.Name} ({prop.PropertyType.Name})");
+                }
+
+                Console.WriteLine("\nMethods->");
+                foreach (var meth in t.GetMethods())
+                {
+                    Console.WriteLine($"Method: {meth.Name}");
+                }
+
+                Console.WriteLine("\nConstructors");
+                foreach (var con in t.GetConstructors())
+                {
+                    Console.WriteLine("Constructor:" + con);
+                }
+
+                Console.WriteLine("\nDyanmic Obj Creation");
+                object obj = Activator.CreateInstance(t);
+                t.GetProperty("Name")!.SetValue(obj, "Dilshad");
+                t.GetProperty("Age")!.SetValue(obj, 22);
+
+                MethodInfo studyMethod = t.GetMethod("Study");
+                studyMethod.Invoke(obj, null);
+
+                MethodInfo showMethod = t.GetMethod("ShowDetails");
+                showMethod.Invoke(obj, null);
+            }
+        }
+    }
+}
