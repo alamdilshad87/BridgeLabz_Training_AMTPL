def8fcccf286533b8c478d5a2e2d8cc826677590
alamdilshad87 <dilshadand@gmail.com>
2025-12-18 15:57:01 +0530

    Extra NUnit

---
+++ BridgeLabZ/BridgeLabZ.NUnitTesting/CalccccDivide/Calcc.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace BridgeLabZ.NUnitTesting.CalculatorTests
+{
+    [TestFixture]
+    public class Calcc
+    {
+        private Calcccc calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calcccc();
+        }
+
+        [Test]
+        public void Div()
+        {
+            int result = calc.Divide1(20, 10);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ BridgeLabZ/BridgeLabZ/Calcccc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabZ
+{
+    public class Calcccc
+    {
+        public int Divide1(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
