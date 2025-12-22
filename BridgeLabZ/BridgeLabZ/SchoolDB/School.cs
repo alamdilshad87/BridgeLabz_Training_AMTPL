79d7d94f91f69e8c1fdc5297be5738f7863f3ce6
alamdilshad87 <dilshadand@gmail.com>
2025-12-22 14:36:50 +0530

    Linked to DB and CRUD Op

---
+++ BridgeLabZ/BridgeLabZ/SchoolDB/School.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BridgeLabZ.SchoolDB
+{
+    internal class School
+    {
+        static void Main()
+        {
+            string connectionString =
+                "Server=DILSHAD\\SQLEXPRESS;" +
+                "Database=SchoolDB;" +
+                "Trusted_Connection=True;" +
+                "TrustServerCertificate=True;";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                Console.WriteLine("Connected to DB");
+
+                SqlCommand insertCmd = new SqlCommand(
+                    "INSERT INTO Students (Name, Age, City) VALUES ('Dilshad', 21, 'Dhanbad')",
+                    con);
+                insertCmd.ExecuteNonQuery();
+                Console.WriteLine("Inserted");
+
+                SqlCommand selectCmd = new SqlCommand(
+                    "SELECT * FROM Students",
+                    con);
+                SqlDataReader reader = selectCmd.ExecuteReader();
+
+                Console.WriteLine("Students:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(
+                        $"Id: {reader["Id"]}, Name: {reader["Name"]}, Age: {reader["Age"]}, City: {reader["City"]}");
+                }
+                reader.Close();
+
+                SqlCommand updateCmd = new SqlCommand(
+                    "UPDATE Students SET City='Ranchi' WHERE Name='Dilshad'",
+                    con);
+                updateCmd.ExecuteNonQuery();
+                Console.WriteLine("Updated");
+
+                //SqlCommand deleteCmd = new SqlCommand(
+                //    "DELETE FROM Students WHERE Name='Dilshad'",
+                //    con);
+                //deleteCmd.ExecuteNonQuery();
+                //Console.WriteLine("Deleted");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
