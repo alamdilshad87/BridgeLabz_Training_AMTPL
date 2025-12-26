8a3a527d4c84ae9d2c3aeb7507d4b3603d5ef82c
alamdilshad87 <dilshadand@gmail.com>
2025-12-26 17:54:12 +0530

    Login Using DB

--- BridgeLabZ/WebFormTesting/LoginDB.aspx.cs
+++ BridgeLabZ/WebFormTesting/LoginDB.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,9 +18,33 @@ namespace WebFormTesting
 
         protected void Login_Click(object sender, EventArgs e)
         {
+            string constr = System.Configuration.ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
             string user = UserNameTxt.Text;
             string pass = PasswordTxt.Text;
 
+            using(SqlConnection conn=new SqlConnection(constr))
+            {
+                string query = "Select count(*) from dbo.Table where user=@u and password=@p";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                // Parameter binding (prevents SQL Injection)
+                cmd.Parameters.AddWithValue("@u", user);
+                cmd.Parameters.AddWithValue("@p", pass);
+
+                conn.Open(); // ACTUAL DB CONNECTION HAPPENS HERE
+
+                int result = (int)cmd.ExecuteScalar();
+
+                if (result == 1)
+                {
+                    Session["User"] = user;
+                    Response.Redirect("Default.aspx");
+                }
+                else
+                {
+                    lblmsg.Text = "Invalid username or password";
+                }
+            }
         }
     }
 }
